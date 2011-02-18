1d1bdc5dfa6c62ea9d0636d5703f961ac7ca4600
nakaji <nakaji126@gmail.com>
2011-02-18 22:04:58 +0900

    move TimeKeeper to Model

--- CostCounter/Model/TimeKeeper.cs
+++ CostCounter/Model/TimeKeeper.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using CostCounter.Model;
 using System.Threading;
 
-namespace CostCounter
+namespace CostCounter.Model
 {
     public class TimeKeeper
     {
