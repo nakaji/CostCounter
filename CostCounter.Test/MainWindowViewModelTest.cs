637dcd7863bec7ce0723ea51f8d569281c41b5f1
nakaji <nakaji126@gmail.com>
2011-02-19 15:35:45 +0900

    ViewからViewModelに変更

---
+++ CostCounter.Test/MainWindowViewModelTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CostCounter.ViewModel;
+using NUnit.Framework;
+
+namespace CostCounter.Test
+{
+    [TestFixture]
+    public class MainWindowViewModelTest
+    {
+        [Test]
+        public void 開始してないと停止コマンドは使えない()
+        {
+            var model = new MainWindowViewModel();
+        }
+    }
+}
---
+++ CostCounter/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,53 @@
+using System.ComponentModel;
+using System.Windows.Input;
+using CostCounter.Model;
+using System;
+
+namespace CostCounter.ViewModel
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        #endregion
+
+        private TimeKeeper _keeper = new TimeKeeper(new Clock(), 60);
+
+        private ICommand _startCommand;
+        //public ICommand StartCommand
+        //{
+        //    get {return _startCommand ?? (_startCommand=new Command(()=>_keeper.Start(), ()=>_keeper.IsRunning)) }
+        //}
+    }
+
+    public class Command : ICommand
+    {
+        private readonly Action _execute;
+        private readonly Func<bool> _canExecute;
+
+        public Command(Action execute, Func<bool> canExecute)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute();
+        }
+
+        public event System.EventHandler CanExecuteChanged;
+
+        public void Execute(object parameter)
+        {
+            _execute();
+        }
+    }
+}
